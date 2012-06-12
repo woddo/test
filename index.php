1455a43643f587014ca79f1a25d9656ed5adcadb
woddo <woddochild@yahoo.com>
2012-06-12 22:36:35 +0200

    test commit

--- index.php
+++ index.php
@@ -1,36 +1,5 @@
 <?php
 
+	print "test";
 
-$polje = array(range(1,20));
-
-$rand = rand(1,30);
-
-if(is_array($a))
-    {
-     print "je array";
-    }
-?>
-
-
-<?php
-	if($x == 12) dsfsdfsd
-	{
-		sdfdssf
-		//kaj sedaj naprej
-	}   
-	
-	function janez()
-	{
-		//probimo
-		
-		//trereerfer
-		
-		print "tole popravimo";
-	}
-	
-	
-	function kajje($a=0)
-	{
-		print 12;
-	}
 ?>
